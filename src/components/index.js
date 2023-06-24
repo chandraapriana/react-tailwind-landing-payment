18a5cf170059faee0f08f6a757ceed9733b3e882
chandraapriana <aprianachandra@gmail.com>
2023-06-24 20:39:42 +0700

    feat: create get started component and Hero component

--- src/components/index.js
+++ src/components/index.js
@@ -8,7 +8,7 @@ import Stats from "./Stats";
 import Footer from "./Footer";
 import Testimonials from "./Testimonials";
 import Hero from "./Hero";
-
+import GetStarted from "./GetStarted";
 export {
   Navbar,
   Billing,
@@ -20,4 +20,5 @@ export {
   Footer,
   Testimonials,
   Hero,
+  GetStarted,
 };
