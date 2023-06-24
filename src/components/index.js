0adf9d0d27d2de72ebd08ae72729af83b12336da
chandraapriana <aprianachandra@gmail.com>
2023-06-24 21:22:28 +0700

    feat: add business section

--- src/components/index.js
+++ src/components/index.js
@@ -9,6 +9,7 @@ import Footer from "./Footer";
 import Testimonials from "./Testimonials";
 import Hero from "./Hero";
 import GetStarted from "./GetStarted";
+import Button from "./Button";
 export {
   Navbar,
   Billing,
@@ -21,4 +22,5 @@ export {
   Testimonials,
   Hero,
   GetStarted,
+  Button,
 };
